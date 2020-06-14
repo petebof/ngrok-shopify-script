0e1ea8e9f8f348de44f3b83dd7d86249a0d4983a
petebof <45689821+petebof@users.noreply.github.com>
2020-06-14 23:24:53 +0300

    Update ngrokshopify.sh

--- ngrokshopify.sh
+++ ngrokshopify.sh
@@ -25,7 +25,7 @@ echo "ngrok ngrok subdomain: $subdomain"
 echo ====================================================
 
 google-chrome $1 1>&- 2>&-
-read -p 'Login to Shopify app admin and press a to abort, other to continue: ' cont
+read -p 'Ensure that you are logged to Shopify app admin. Press a to abort, other to continue: ' cont
 if [ "$cont" = 'a' ]
 then
   exit 2
